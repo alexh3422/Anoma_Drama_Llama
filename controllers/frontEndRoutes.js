dfdbc6ed6e8af264233981794da5b0bbbce93ff1
dejesusf <dejesusf@uw.edu>
2023-02-10 10:08:22 -0800

    idk what i changed?

--- controllers/frontEndRoutes.js
+++ controllers/frontEndRoutes.js
@@ -23,9 +23,11 @@ router.get("/", (req, res) => {
 router.get("/login", (req, res) => {
   res.render("login");
 });
+
 router.get("/signup", (req, res) => {
   res.render("signup");
 });
+
 router.get("/home", (req, res) => {
   res.render("home");
 })
