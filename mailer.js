f7d5f7dc508379d13f52a38779e71e9f8b3821a0
Alex Hall <alexhallinthecloud@icloud.com>
2023-02-10 12:41:06 -0800

    installed nodemailer package

---
+++ mailer.js
@@ -0,0 +1,44 @@
+const nodemailer = require("nodemailer");
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    auth: {
+      user: "leif.reichert40@ethereal.email",
+      pass: "ZTZjQ13KpnuG4qxAbw",
+    },
+  });
+
+$mail = new PHPMailer(true);
+$mail->isSMTP();
+$mail->Host = 'smtp.ethereal.email';
+$mail->SMTPAuth = true;
+$mail->Username = 'leif.reichert40@ethereal.email';
+$mail->Password = 'ZTZjQ13KpnuG4qxAbw';
+$mail->SMTPSecure = 'tls';
+$mail->Port = 587;
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <leif.reichert40@ethereal.email>', // sender address
+    to: "alexh25@uw.edu, alex", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+main().catch(console.error);
