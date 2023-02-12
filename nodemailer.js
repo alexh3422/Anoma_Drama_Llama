0eae99fd1ef60a5a875c2e3f08b9587e5a579a1b
Alex Hall <alexhallinthecloud@icloud.com>
2023-02-12 11:38:09 -0800

    nodemailer set up

--- nodemailer.js
+++ nodemailer.js
@@ -1,3 +1,28 @@
-let transporter = nodemailer.createTransport(options[defaults])
+("use strict");
+const nodemailer = require("nodemailer");
 
+async function main() {
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "DramaLlamaCEO@gmail.com",
+      pass: "xamzjbohifbajihy",
+      tls: {
+        rejectUnAuthorized: true,
+      },
+    },
+  });
 
+  let info = await transporter.sendMail({
+    from: '"DramaLlamaCEO@DramaLlama.com', // sender address
+    to: "alexander.hallpnw@icloud.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+}
+
+main().catch(console.error);
