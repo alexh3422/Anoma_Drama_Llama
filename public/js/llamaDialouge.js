bd7ff929df3dedde887d3e4e8a0e5d90769c378f
William <wabarringer@gmail.com>
2023-02-14 13:44:55 -0800

    update

--- public/js/llamaDialouge.js
+++ public/js/llamaDialouge.js
@@ -3,6 +3,7 @@ const dialougeArray = ["Yum!", "Thanks! That was tasty!", "Sooo delicious!"];
 const validateBtn2 = document.querySelector("#validateBtn");
 const trackMoodBtn2 = document.querySelector("#trackMoodBtn");
 const journalBtn = document.querySelector("#submit-post");
+const deleteBtn2 = document.querySelector("#deleteBtn");
 
 if (window.location.pathname === "/mood") {
   validateBtn2.addEventListener("click", (event) => {
@@ -15,6 +16,7 @@ if (window.location.pathname === "/mood") {
     }, 10000);
   });
   trackMoodBtn2.addEventListener("click", (event) => {
+    event.preventDefault();
     document.querySelector("#speechBubble").innerHTML =
       "How are you feeling? You can choose as many moods as you'd like, then just click outside of the wheel when you're finished.";
     document.querySelector(".bubble").style.display = "block";
