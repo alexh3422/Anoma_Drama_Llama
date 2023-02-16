d0bba6a1ad382e6c84e642be273bb48b8ad1415b
Maya Lynn <mayalynncohen@gmail.com>
2023-02-15 17:21:42 -0800

    header feature done

--- public/js/userInfo.js
+++ public/js/userInfo.js
@@ -14,59 +14,59 @@ fetch('/sessions', {
             userNameandInfo.textContent = `Hi ${data.username}`
             if(data.currentMood){
             moodInfo.textContent = `Your current mood is: ${data.currentMood}`
-            
+            const emoji = document.querySelector("#emoji")
             const boxes = document.querySelectorAll('.row')
             boxes.forEach(box => {
                 box.setAttribute("class", `row ${data.currentMood}`)
             });
         
             if(data.currentMood==="tired"){
-                userNameandInfo.textContent = `Hi ${data.username} 😴`
+                emoji.textContent = `😴`
                 // background.style.background = '#49adb4'
             } else if(data.currentMood==="happy"){
-                userNameandInfo.textContent = `Hi ${data.username} 😀`
+                emoji.textContent = `😀`
                 // background.style.background = '#479b79'
             } else if(data.currentMood==="annoyed"){
-                userNameandInfo.textContent = `Hi ${data.username} 😒`
+                emoji.textContent = `😒`
                 // background.style.background = '#47c14d'
             } else if(data.currentMood==="embarrassed"){
-                userNameandInfo.textContent = `Hi ${data.username} 😳`
+                emoji.textContent = `😳`
                 // background.style.background = '#47c14d'
             } else if(data.currentMood==="sad"){
-                userNameandInfo.textContent = `Hi ${data.username} 😢`
+                emoji.textContent = `😢`
                 // background.style.background = '#47c14d'
             } else if(data.currentMood==="calm"){
-                userNameandInfo.textContent = `Hi ${data.username} 😌`
+                emoji.textContent = `😌`
                 // background.style.background = '#47c14d'
             } else if(data.currentMood==="bored"){
-                userNameandInfo.textContent = `Hi ${data.username} 😐`
+                emoji.textContent = `😐`
                 // background.style.background = '#47c14d'
             } else if(data.currentMood==="disappointed"){
-                userNameandInfo.textContent = `Hi ${data.username} 😕`
+                emoji.textContent = `😕`
                 // background.style.background = '#47c14d'
             } else if(data.currentMood==="mischievous"){
-                userNameandInfo.textContent = `Hi ${data.username} 😈`
+                emoji.textContent = `😈`
                 // background.style.background = '#47c14d'
             } else if(data.currentMood==="angry"){
-                userNameandInfo.textContent = `Hi ${data.username} 😡`
+                emoji.textContent = `😡`
                 // background.style.background = '#47c14d'
             } else if(data.currentMood==="flirty"){
-                userNameandInfo.textContent = `Hi ${data.username} 😘`
+                emoji.textContent = `😘`
                 // background.style.background = '#47c14d'
             } else if(data.currentMood==="anxious"){
-                userNameandInfo.textContent = `Hi ${data.username} 😟`
+                emoji.textContent = `😟`
                 // background.style.background = '#47c14d'
             } else if(data.currentMood==="silly"){
-                userNameandInfo.textContent = `Hi ${data.username} 😛`
+                emoji.textContent = `😛`
                 // background.style.background = '#47c14d'
             } else if(data.currentMood==="scared"){
-                userNameandInfo.textContent = `Hi ${data.username} 😧`
+                emoji.textContent = `😧`
                 // background.style.background = '#47c14d'
             } else if(data.currentMood==="frustrated"){
-                userNameandInfo.textContent = `Hi ${data.username} 😫`
+                emoji.textContent = `😫`
                 // background.style.background = '#47c14d'
             } else if(data.currentMood==="excited"){
-                userNameandInfo.textContent = `Hi ${data.username} 🤩`
+                emoji.textContent = `🤩`
                 // background.style.background = '#47c14d'
             }
            } 
